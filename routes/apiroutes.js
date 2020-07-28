2dbfcd4c395d27dd8ca37468b3cc1708cf7079b1
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-28 08:37:20 -0400

    thirt times the charm, creating new repo to fix issues

---
+++ routes/apiroutes.js
@@ -0,0 +1,30 @@
+const db = require('../models');
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then((allWorkouts) => {
+        res.json(allWorkouts);
+    });
+});
+
+
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body).then(newWorkout => {
+      res.json(newWorkout)
+  }).catch(err => {
+      console.log(err);
+      res.json({
+          error: true,
+          data: null,
+          message: "error"
+      })
+  });
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const htmlRoutes = require('./routes/htmlroutes.js');
+const apiRoutes = require('./routes/apiroutes.js');
+
+
+const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { 
+    useNewUrlParser: true, useUnifiedTopology:true 
+});
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log('Mongoose connected.');
+});
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`)
+});
